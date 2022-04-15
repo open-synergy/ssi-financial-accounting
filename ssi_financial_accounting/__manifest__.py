a1134c66831363d780f99b013862885f11bc8d55
Andhitia Rama <andhitia.r@gmail.com>
2022-04-15 14:58:46 +0700

    [ssi_financial_accounting] 14.0.1.0.0

---
+++ ssi_financial_accounting/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Financial Accounting",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "account",
+        "configuration_helper",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "menu.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "demo": [],
+}
