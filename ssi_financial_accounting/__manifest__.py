c4c0b9d6d655943fb53c00e085bb9b1cdf0ec0e2
Andhitia Rama <andhitia.r@gmail.com>
2022-05-15 06:09:35 +0700

    [ssi_financial_accounting] 14.0.2.0.0
    * Add dependencies

--- ssi_financial_accounting/__manifest__.py
+++ ssi_financial_accounting/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Financial Accounting",
-    "version": "14.0.1.5.0",
+    "version": "14.0.2.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
@@ -14,12 +14,19 @@
         "account",
         "configuration_helper",
         "currency_rate_inverted",
+        "account_financial_report",
+        "mis_builder",
+        "mis_template_financial_report",
     ],
     "data": [
         "security/ir_module_category_data.xml",
         "security/res_group_data.xml",
         "menu.xml",
         "views/res_config_settings_views.xml",
+        "views/mis_report_instance_views.xml",
+        "wizards/general_ledger_wizard_views.xml",
+        "wizards/trial_balance_wizard_views.xml",
+        "wizards/aged_partner_balance_wizard_views.xml",
     ],
     "demo": [],
 }
