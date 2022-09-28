b7f63d15273e123f7e9da308cb6eee7c071347b0
Andhitia Rama <andhitia.r@gmail.com>
2022-09-28 18:26:15 +0700

    [ssi_account_move_py3o_report] 14.0.1.0.0

---
+++ ssi_account_move_py3o_report/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Py3o Report for Journal Entry",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "ssi_financial_accounting",
+        "report_py3o",
+        "base_usability",
+    ],
+    "data": [
+        "reports/account_move_report.xml",
+    ],
+    "demo": [],
+}
