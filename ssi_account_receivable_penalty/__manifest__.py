b7047481df7b8be2aad0464cf62883ecd35e51f0
mikevhe18 <viriyananda.michael@gmail.com>
2022-12-14 15:56:52 +0700

    [ssi_account_receivable_penalty] 14.0.1.0.0 Add dependencies base_automation

--- ssi_account_receivable_penalty/__manifest__.py
+++ ssi_account_receivable_penalty/__manifest__.py
@@ -17,6 +17,7 @@
         "ssi_transaction_done_mixin",
         "ssi_transaction_cancel_mixin",
         "ssi_company_currency_mixin",
+        "base_automation",
     ],
     "data": [
         "security/ir_module_category_data.xml",
