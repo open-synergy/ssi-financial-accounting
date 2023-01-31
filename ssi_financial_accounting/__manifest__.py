aac7a6ed29a0e6c3f348b04d023a74b96c9a6bee
Andhitia Rama <andhitia.r@gmail.com>
2023-01-31 17:43:28 +0700

    [ssi_financial_accounting] 14.0.2.9.0

--- ssi_financial_accounting/__manifest__.py
+++ ssi_financial_accounting/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Financial Accounting",
-    "version": "14.0.2.8.0",
+    "version": "14.0.2.9.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
@@ -17,10 +17,12 @@
         "account_financial_report",
         "mis_builder",
         "mis_template_financial_report",
+        "ssi_master_data_mixin",
     ],
     "data": [
         "security/ir_module_category_data.xml",
         "security/res_group_data.xml",
+        "security/ir.model.access.csv",
         "data/account_journal_data.xml",
         "menu.xml",
         "views/res_config_settings_views.xml",
@@ -28,7 +30,10 @@
         "views/mis_report_style_views.xml",
         "views/mis_report_views.xml",
         "views/account_account_type_views.xml",
+        "views/account_account_views.xml",
+        "views/account_move_line_views.xml",
         "views/account_move_views.xml",
+        "views/cash_flow_type_views.xml",
         "wizards/general_ledger_wizard_views.xml",
         "wizards/trial_balance_wizard_views.xml",
         "wizards/aged_partner_balance_wizard_views.xml",
