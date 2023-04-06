68af1a109f534c9f3faabcf43e4d10dd758b9237
Miftahussalam <miftah_salam@ymail.com>
2023-04-06 15:27:45 +0700

    [ssi_financial_accounting] 14.0.3.0.0

--- ssi_financial_accounting/wizards/aged_partner_balance_report_wizard.py
+++ ssi_financial_accounting/wizards/aged_partner_balance_report_wizard.py
@@ -5,10 +5,25 @@ from odoo.exceptions import ValidationError
 class AgedPartnerBalanceReportWizard(models.TransientModel):
     _inherit = 'aged.partner.balance.report.wizard'
 
+    use_auto_interval = fields.Boolean(
+        string='Automatic Interval',
+        default=True)
     due_interval = fields.Integer(
         string='Due Interval',
-        required=True,
+        required=False,
         default=30)
+    interval_1 = fields.Integer(
+        string='Interval I',
+        required=False)
+    interval_2 = fields.Integer(
+        string='Interval II',
+        required=False)
+    interval_3 = fields.Integer(
+        string='Interval III',
+        required=False)
+    interval_4 = fields.Integer(
+        string='Interval IV',
+        required=False)
 
     @api.constrains('due_interval')
     def validate_due_interval(self):
@@ -17,13 +32,22 @@ class AgedPartnerBalanceReportWizard(models.TransientModel):
                 raise ValidationError(_('Due interval must be greather than zero.'))
 
     def get_interval_label(self):
-        interval_labels = {}
-        for x in range(5):
-            if x < 4:
-                label = f'{(x * self.due_interval + 1)} - {((x + 1) * self.due_interval)} d.'
-            else:
-                label = f'> {(x * self.due_interval)} d.'
-            interval_labels[x] = label
+        if self.use_auto_interval:
+            interval_labels = {}
+            for x in range(5):
+                if x < 4:
+                    label = f'{(x * self.due_interval + 1)} - {((x + 1) * self.due_interval)} d.'
+                else:
+                    label = f'> {(x * self.due_interval)} d.'
+                interval_labels[x] = label
+        else:
+            interval_labels = {
+                0: f'1 - {self.interval_1} d.',
+                1: f'{(self.interval_1 + 1)} - {self.interval_2} d.',
+                2: f'{(self.interval_2 + 1)} - {self.interval_3} d.',
+                3: f'{(self.interval_3 + 1)} - {self.interval_4} d.',
+                4: f'> {self.interval_4} d.',
+            }
         return {
             'due_interval': self.due_interval,
             'interval_label1': interval_labels[0],
@@ -45,3 +69,11 @@ class AgedPartnerBalanceReportWizard(models.TransientModel):
             'interval_label5': interval_labels['interval_label5'],
         })
         return res
+
+    def get_interval(self, index):
+        self.ensure_one()
+        if self.use_auto_interval:
+            interval = self.due_interval * index
+        else:
+            interval = getattr(self, f'interval_{index}')
+        return interval
