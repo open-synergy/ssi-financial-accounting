4ccdf2961af59681f27cc32a2e9f7a67e19cfc37
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-31 13:18:46 +0700

    [ssi_account_move_sequence] Publish to Pypi

--- ssi_account_move_sequence/__manifest__.py
+++ ssi_account_move_sequence/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Sequence Mixin Implementation on Journal Entry",
-    "version": "14.0.1.2.0",
+    "version": "14.0.1.2.1",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "license": "LGPL-3",
