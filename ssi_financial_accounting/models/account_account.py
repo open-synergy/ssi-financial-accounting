04f16c28d849a012a88772e55c268eb167eaffd7
Andhitia Rama <andhitia.r@gmail.com>
2023-10-10 16:13:09 +0700

    [UPD] ssi_financial_accounting
    * Add account type data (different from user type from odoo). This due account type will be dropped on v16
    * Rename odoo's account type into Odoo Account Type
    * Reconfigure account based on account type, not odoo account type

--- ssi_financial_accounting/models/account_account.py
+++ ssi_financial_accounting/models/account_account.py
@@ -2,7 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountAccount(models.Model):
@@ -22,3 +22,38 @@ class AccountAccount(models.Model):
             ("kind", "=", "indirect"),
         ],
     )
+    type_id = fields.Many2one(
+        string="Type",
+        comodel_name="account_type",
+    )
+
+    user_type_id = fields.Many2one(
+        string="Odoo Type",
+    )
+
+    @api.onchange("type_id")
+    def onchange_user_type_id(self):
+        self.user_type_id = False
+        if self.type_id:
+            self.user_type_id = self.type_id.odoo_type_id
+
+    @api.onchange("type_id")
+    def onchange_reconcile(self):
+        self.reconcile = False
+        if self.type_id:
+            self.reconcile = self.type_id.reconcile
+
+    @api.onchange("type_id")
+    def onchange_direct_cash_flow_type_id(self):
+        self.direct_cash_flow_type_id = False
+        if self.type_id:
+            self.direct_cash_flow_type_id = self.type_id.direct_cash_flow_type_id
+
+    @api.onchange("type_id")
+    def onchange_indirect_cash_flow_type_id(self):
+        self.indirect_cash_flow_type_id = False
+        if self.type_id:
+            self.indirect_cash_flow_type_id = self.type_id.indirect_cash_flow_type_id
+
+    def _onchange_user_type_id(self):
+        return True
