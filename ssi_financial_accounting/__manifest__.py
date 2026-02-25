32307c5ab79c67997286330e5ed0439a8e317438
Andhitia Rama <andhitia.r@gmail.com>
2026-02-25 23:30:33 +0700

    [UPD]
    * Memisahkan res.groups untuk journal entry

--- ssi_financial_accounting/__manifest__.py
+++ ssi_financial_accounting/__manifest__.py
@@ -32,6 +32,7 @@
     "data": [
         "security/ir_module_category_data.xml",
         "security/res_group_data.xml",
+        "security/res_groups/journal_entry.xml",
         "security/ir.model.access.csv",
         "data/account_journal_data.xml",
         "data/account_type_data.xml",
