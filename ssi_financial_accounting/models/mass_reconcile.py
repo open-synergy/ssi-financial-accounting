fa6d2495e4d50b98c537d4e919cd8d5b95f82a00
Andhitia Rama <andhitia.r@gmail.com>
2026-04-08 20:00:19 +0700

    [UPD] ssi_financial_accounting
    * Menambahkan filter berdasarkan partner domain

--- ssi_financial_accounting/models/mass_reconcile.py
+++ ssi_financial_accounting/models/mass_reconcile.py
@@ -2,11 +2,23 @@
 # Copyright 2026 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import fields, models
 
 
 class AccountMassReconcileMethod(models.Model):
     _inherit = "account.mass.reconcile.method"
+
+    filter_partner = fields.Boolean(
+        string="Filter Partner",
+        help="If checked, only move lines belonging to partners matching "
+        "the Partner Domain will be considered for reconciliation.",
+    )
+    partner_domain = fields.Text(
+        string="Partner Domain",
+        help="Domain expression to filter partners for reconciliation. "
+        "Only move lines belonging to partners matching this domain "
+        "will be considered during the Advanced Partner Only reconciliation method.",
+    )
 
     def _selection_name(self):
         methods = super()._selection_name()
@@ -17,3 +29,13 @@ class AccountMassReconcileMethod(models.Model):
             ),
         ]
         return methods
+
+
+class AccountMassReconcile(models.Model):
+    _inherit = "account.mass.reconcile"
+
+    def _prepare_run_transient(self, rec_method):
+        vals = super()._prepare_run_transient(rec_method)
+        vals["filter_partner"] = rec_method.filter_partner
+        vals["partner_domain"] = rec_method.partner_domain
+        return vals
