5421556413489cb8251f57439c120b9339659bfd
Andhitia Rama <andhitia.r@gmail.com>
2026-05-20 13:52:41 +0700

    [UPD] ssi_account_move_line_day_overdue
    * Fix xml-deprecated-data-node: use <odoo noupdate="1"> instead of <odoo><data>
    * Add unit tests

---
+++ ssi_account_move_line_day_overdue/tests/test_ssi_account_move_line_day_overdue.py
@@ -0,0 +1,13 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestSsiAccountMoveLineDayOverdue(YamlTransactionCase):
+    def test_ssi_account_move_line_day_overdue(self):
+        self.run_yaml_scenario("test_data_ssi_account_move_line_day_overdue.yaml")
